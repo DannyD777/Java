4eadb2851cc5a59c5e41c3506de6e150f3179072
DannyD777 <den.danik777@icloud.com>
2023-10-02 00:59:33 +0300

    Java

---
+++ src/p5/Main.java
@@ -0,0 +1,43 @@
+package p5;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Phone apple = new Phone();
+        apple.printInfo();
+        apple.model = "Apple";
+        apple.number = 895377777;
+        apple.weight = 194;
+        apple.printInfo();
+        apple.getNumber();
+        apple.printInfo();
+
+
+
+        Phone apple1  = new Phone("ApMax",355433222,231);
+        apple1.printInfo();
+
+        Phone apple2  = new Phone("ApPro",355678898,189);
+        apple2.printInfo();
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
---
+++ src/p5/Phone.java
@@ -0,0 +1,63 @@
+package p5;
+
+
+public class Phone {
+
+
+
+    String model;
+    int number;
+    int weight;
+
+
+
+
+    {
+        System.out.println("\n Start init object");
+
+    }
+
+    public Phone() {
+
+    this("none", 0,0);
+        System.out.println("-----------------------------------");
+
+    }
+
+
+
+    public Phone(String model, int number, int weight) {
+         this.model = model;
+         this.number = number;
+         this.weight = weight;
+        System.out.println("-----------------------------------");
+    }
+
+    public Phone(String model, int number) {
+        this.model = model;
+        this.number = number;
+        System.out.println("-----------------------------------");
+    }
+
+    static void receiveCall(){
+
+        System.out.println("Звонит: \n ");
+        System.out.println("-----------------------------------");
+
+
+    }
+
+
+    public int getNumber() {
+        System.out.println("Number: " + number);
+        System.out.println("-----------------------------------");
+        return number;
+
+    }
+    void printInfo(){
+        System.out.printf(" Model:  %s \n Number: %d \n Weight: %d \n", model, number, weight );
+        System.out.println("-----------------------------------");
+    }
+}
+
+
